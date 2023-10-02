c02ba88a2c495cc5cd49c215e20353299b6eda75
E8Vitel <viktoralvz@gmail.com>
2023-10-02 16:58:58 -0300

    Navigation Drawer now working

--- app/src/main/java/com/example/taskmaster/SesionActivity.java
+++ app/src/main/java/com/example/taskmaster/SesionActivity.java
@@ -21,6 +21,7 @@ public class SesionActivity extends AppCompatActivity {
                     public void onClick(View view) {
                 Intent intent = new Intent(SesionActivity.this,TareaActivity.class);
                 startActivity(intent);
+
             }
         });
     }
