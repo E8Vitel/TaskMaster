5b0cb76bdf5bbfedfc8c80199b3ffdae2aa02243
E8Vitel <viktoralvz@gmail.com>
2023-11-04 00:01:09 -0300

    -Nuevo XML de un Alert Dialog (Pop-up) -Metodo para usar un EditText como botón

--- app/src/main/java/com/example/taskmaster/TareaActivity.java
+++ app/src/main/java/com/example/taskmaster/TareaActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.Activity;
-import android.content.ComponentName;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -18,23 +22,40 @@ public class TareaActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ImageView menu;
     LinearLayout home, settings, about, logout;
+    Button btnAgregarTask;
+    EditText txtFechaLimite;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarea);
 
-        drawerLayout = findViewById(R.id.drawerLayout);
         menu = findViewById(R.id.menu);
         home = findViewById(R.id.home);
         about = findViewById(R.id.abouts);
         logout = findViewById(R.id.exit);
         settings = findViewById(R.id.settings);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        btnAgregarTask = findViewById(R.id.btnAgregarTask);
+        txtFechaLimite = findViewById(R.id.txtFechaLimite);
+
+        // Dentro del método onClick del botón de agregar, por ejemplo
+
+        btnAgregarTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddTaskDialog();
+            }
+        });
+
+
 
         menu.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               openDrawer(drawerLayout);
-           }
+            @Override
+            public void onClick(View view) {
+                openDrawer(drawerLayout);
+            }
         });
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,14 +66,14 @@ public class TareaActivity extends AppCompatActivity {
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(TareaActivity.this,ConfigActivity.class);
+                Intent intent = new Intent(TareaActivity.this, ConfigActivity.class);
                 startActivity(intent);
             }
         });
         about.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(TareaActivity.this,AboutActivity.class);
+                Intent intent = new Intent(TareaActivity.this, AboutActivity.class);
                 startActivity(intent);
             }
         });
@@ -63,11 +84,13 @@ public class TareaActivity extends AppCompatActivity {
             }
         });
     }
+
     public static void openDrawer(DrawerLayout drawerLayout) {
         drawerLayout.openDrawer(GravityCompat.START);
     }
+
     public static void closeDrawer(DrawerLayout drawerLayout) {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
         }
     }
@@ -77,4 +100,49 @@ public class TareaActivity extends AppCompatActivity {
         super.onPause();
         closeDrawer(drawerLayout);
     }
+    private void showAddTaskDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.custom_dialog_layout, null);
+        builder.setView(view);
+
+        // Configura el AlertDialog
+        builder.setTitle("Agregar Tarea")
+                .setPositiveButton("Agregar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Aquí puedes manejar la lógica de agregar la tarea
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        // Agregar lógica para mostrar el DatePickerDialog al tocar el EditText de fecha
+        txtFechaLimite = view.findViewById(R.id.txtFechaLimite);
+        txtFechaLimite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+    }
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                // Aquí puedes manejar la fecha seleccionada
+                String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+                txtFechaLimite.setText(selectedDate);
+            }
+        }, 2023, 11, 3); // Ajusta el año, mes y día según sea necesario
+
+        datePickerDialog.show();
+    }
+
 }
